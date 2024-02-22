637288057d776f53ee31e964a3ee9b776d0ee845
Hana1131 <mejdoub.hana18@gmail.com>
2024-02-22 12:27:53 +0100

    Préparation BD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule, DatePipe } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { AfficherTicketrComponent } from './components/afficher-ticket/afficher-ticketr.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { AjouterticketComponent } from './components/ajouterticket/ajouterticket.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { UpdateticketComponent } from './components/updateticket/updateticket.component';
-import { ToastrModule } from 'ngx-toastr';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { ChatComponent } from './components/chat/chat.component';
+import { MessageComponent } from './components/message/message.component';
+import { MetricComponent } from './components/metric/metric.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AfficherTicketrComponent,
-    AjouterticketComponent,
-    UpdateticketComponent,
+    ChatComponent,
+    MessageComponent,
+    MetricComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    ToastrModule.forRoot(),
-    BrowserAnimationsModule // ToastrModule added
-    
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [DatePipe],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
